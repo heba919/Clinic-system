58022ceb207a2507bd1dcdaf63b081cd4fd24a37
habe <waledheba91@gmail.com>
2020-12-26 21:51:17 +0200

    add project

---
+++ Clinic_system/src/Collage_main/Clinic_system.java
@@ -0,0 +1,14 @@
+
+package Collage_main;
+
+
+public class Clinic_system {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
