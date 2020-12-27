5bd4b1788f6c25dff7b2cfd3a07b6c29d99fdca7
Mahmoud-kamal12 <mahmoud192551@fci.bu.edu.eg>
2020-12-27 12:18:46 +0200

    add mysqlconector jar

--- Clinic_system/src/Function_database/Connect.java
+++ Clinic_system/src/Function_database/Connect.java
@@ -1,14 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Function_database;
+
+import com.mysql.jdbc.Connection;
+import java.sql.*;
+
 
 /**
  *
- * @author pc
+ * @author aa
  */
 public class Connect {
+   public String dbname = "";
+   public String dbURL = "jdbc:mysql://localhost:3306/"+dbname; // conest 
+   public String username ="root"; 
+   public String password = "root";  
+    
+    public  Connection conn() throws SQLException
+    {
+        Connection dbcon = null;
+    try
+    {
+        dbcon = (Connection) DriverManager.getConnection(this.dbURL, this.username, this.password);
+    }catch (SQLException se)
+     {
+         throw se;
+         
+     }
+     
+        return dbcon;
+    }
     
 }
