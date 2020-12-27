e5706de82d239fb450663a16edd6a2acb57b6645
habe <waledheba91@gmail.com>
2020-12-27 13:00:14 +0200

    add patient

--- Clinic_system/src/Class/Patient.java
+++ Clinic_system/src/Class/Patient.java
@@ -1,14 +1,107 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Class;
 
-/**
- *
- * @author pc
- */
 public class Patient {
+    private int patient_id;
+    private String name;
+    private int phone ;
+    private String email ;
+    private String address;
+    private String  symptom;
+    private int age ;
+    private int national_id;
+    private String treatment ;
+    private String date;
+    private String info_about_illness;
+
+    public String getTreatment() {
+        return treatment;
+    }
+
+    public void setTreatment(String treatment) {
+        this.treatment = treatment;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getInfo_about_illness() {
+        return info_about_illness;
+    }
+
+    public void setInfo_about_illness(String info_about_illness) {
+        this.info_about_illness = info_about_illness;
+    }
+    
+
+    public int getPatient_id() {
+        return patient_id;
+    }
+
+    public void setPatient_id(int patient_id) {
+        this.patient_id = patient_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getSymptom() {
+        return symptom;
+    }
+
+    public void setSymptom(String symptom) {
+        this.symptom = symptom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getNational_id() {
+        return national_id;
+    }
+
+    public void setNational_id(int national_id) {
+        this.national_id = national_id;
+    }
+    
     
 }
